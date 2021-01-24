598bd8bdb0e1d48f86902609329ab723791e303f
miu200521358 <garnet200521358@gmail.com>
2021-01-24 12:45:46 +0900

    とりあえず動かすまで

--- apps/eval.py
+++ apps/eval.py
@@ -3,6 +3,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 import time
 import json
